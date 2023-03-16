6f988f8d959255355f42789783280da7b64dbe93
einvolko <124045476+einvolko@users.noreply.github.com>
2023-03-16 11:56:48 +0300

    Initial Commit

---
+++ myPizzaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPizzaApp
+//
+//  Created by Михаил Супрун on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
