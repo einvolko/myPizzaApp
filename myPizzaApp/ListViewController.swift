044a35db025ac7794faf0a761702dcb014e392aa
einvolko <124045476+einvolko@users.noreply.github.com>
2023-03-18 13:48:20 +0300

    v1.0.12

--- myPizzaApp/ListViewController.swift
+++ myPizzaApp/ListViewController.swift
@@ -13,6 +13,7 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
         didSet{
             DispatchQueue.main.async {
                 self.pizzaCollectionView.reloadData()
+                self.rollCollectionView.reloadData()
             }
         }
     }
@@ -20,15 +21,15 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
     
     
     @IBOutlet weak var pizzaCollectionView: UICollectionView!
-//    @IBOutlet weak var rollCollectionView: UICollectionView!
+    @IBOutlet weak var rollCollectionView: UICollectionView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pizzaCollectionView.dataSource = self
         pizzaCollectionView.delegate = self
-//        rollCollectionView.dataSource = self
-//        rollCollectionView.dataSource = self
+        rollCollectionView.dataSource = self
+        rollCollectionView.dataSource = self
         fetchData()
         pizzaCollectionView.backgroundView = nil
         pizzaCollectionView.backgroundColor = UIColor .clear
@@ -44,11 +45,13 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        if collectionView == self.pizzaCollectionView{
-        config?.pizzaList[0].count ?? 0
+        if collectionView == self.pizzaCollectionView{
+           return config?.pizzaList[0].count ?? 0
+        } else {
+            return config?.pizzaList[1].count ?? 0
         }
-//        return config?.rollList.count ?? 0
-    
+    }
+        
         func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             if collectionView == self.pizzaCollectionView {
                 let cellPizza = collectionView.dequeueReusableCell(withReuseIdentifier: "PizzaCollectionViewCell", for: indexPath)
@@ -64,24 +67,25 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
                     pizzaCell.pizzaImageView.kf.setImage(with: url, options:[ .processor(processor)])
                 }
                 return cellPizza
-//            } else {
-//                let cellRoll = collectionView.dequeueReusableCell(withReuseIdentifier: "RollCollectionViewCell", for: indexPath)
-//                if
-//                    let rollCell = cellRoll as? RollCollectionViewCell,
-//                    let roll = config?.pizzaList[indexPath.row] {
-//
-//                    rollCell.rollNameLabel.text = roll.rollName
-//                    rollCell.rollDescriptionLabel.text = roll.rollDescription
-//                    rollCell.rollCostLabel.text = roll.rollCost
-//                    let url = URL(string: roll.rollImage)
-//                    let processor = RoundCornerImageProcessor(cornerRadius: 200)
-//                    rollCell.rollImageView.kf.indicatorType = .activity
-//                    rollCell.rollImageView.kf.setImage(with: url, options:[ .processor(processor)])
-//                }
-//                return cellRoll
-//            }
+            } else {
+                let cellRoll = collectionView.dequeueReusableCell(withReuseIdentifier: "RollCollectionViewCell", for: indexPath)
+                if
+                    let rollCell = cellRoll as? RollCollectionViewCell,
+                    let roll = config?.pizzaList[1][indexPath.row] {
+                    
+                    rollCell.rollNameLabel.text = roll.pizzaName
+                    rollCell.rollDescriptionLabel.text = roll.pizzaDescription
+                    rollCell.rollCostLabel.text = String(roll.pizzaCost)
+                    let url = URL(string: roll.pizzaImage)
+                    let processor = RoundCornerImageProcessor(cornerRadius: 200)
+                    rollCell.rollImageView.kf.indicatorType = .activity
+                    rollCell.rollImageView.kf.setImage(with: url, options:[ .processor(processor)])
+                }
+                return cellRoll
+            }
             
         }
         
+        
     }
-}
+
